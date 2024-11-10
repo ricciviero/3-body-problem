ad2ed007a98c4f968b54009d4566d2ae5ee6a767
Riccardo Civiero <riccardociviero98@icloud.com>
2024-11-10 19:20:12 +0100

    tailwind css init

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./*.html"], // Modifica i percorsi in base alla struttura del tuo progetto
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
